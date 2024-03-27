908a2e31d5f675e138fc337b5dca267c40775838
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-03-27 12:13:09 +0500

    add another public folder and use it in AuthLayout

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -1,5 +1,22 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img className="hidden xl:block h-screen bg-no-repeat w-1/2 object-cover" src="assets/images/side-img.svg" alt="beautiful image" />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
--- src/_auth/forms/SignInForm/SignUpForm.tsx
+++ src/_auth/forms/SignInForm/SignUpForm.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default SignUpForm;
