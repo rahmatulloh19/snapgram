0fa00d11f5513a49c3dbdc6d601a26fdda2b31b6
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-04-13 18:39:12 +0500

    edit style

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,11 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
-          <img className="hidden xl:block h-screen bg-no-repeat w-1/2 object-cover" src="assets/images/side-img.svg" alt="beautiful image" />
+          <img
+            className="hidden xl:block h-screen bg-no-repeat w-1/2 object-cover"
+            src="assets/images/side-img.svg"
+            alt="beautiful image"
+          />
         </>
       )}
     </>
